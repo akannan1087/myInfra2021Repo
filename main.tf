21dd9f8c5f57aa18c80b8619543a07ded0699b18
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2021-06-29 11:51:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+#Create security group with firewall rules
+resource "aws_security_group" "security_jenkins_port" {
+  name        = "security_jenkins_port"
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "security_jenkins_port"
+  }
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = "ami-0b9064170e32bde34"
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [ "security_jenkins_port"]
+  tags= {
+    Name = "jenkins_instance"
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "jenkins_elstic_ip"
+  }
+}
