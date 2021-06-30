20c422cff8aa0e9f51ebc86970a0e2b3491f1483
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2021-06-30 14:18:25 -0500

    added variable for security group

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 
 #Create security group with firewall rules
-resource "aws_security_group" "security_jenkins_port" {
-  name        = "security_jenkins_port"
+resource "aws_security_group" "security_jenkins_group" {
+  name        = var.security_group
   description = "security group for jenkins"
 
   ingress {
@@ -31,7 +31,7 @@ resource "aws_security_group" "security_jenkins_port" {
   }
 
   tags= {
-    Name = "security_jenkins_port"
+    Name = var.security_group
   }
 }
 
@@ -39,7 +39,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = "ami-0b9064170e32bde34"
   key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [ "security_jenkins_port"]
+  security_groups= [var.security_group]
   tags= {
     Name = "jenkins_instance"
   }
