f817abd4c18e05588794b320aa92af9ac16bf62d
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2021-07-01 14:56:54 -0500

    added variable for ubuntu 18.0.4 ami id

--- variables.tf
+++ variables.tf
@@ -22,3 +22,8 @@ variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
   default     = "my-ec2-instance"
 }
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0b9064170e32bde34"
+}
