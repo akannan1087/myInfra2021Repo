f1557431efe279adf9276b44d6c5079cf94dc3bd
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2021-12-26 07:20:59 -0600

    added a few more variables

--- variables.tf
+++ variables.tf
@@ -1,29 +1,51 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "us-east-2"
+       description = "The AWS region to create things in." 
+       default     = "us-east-2" 
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "myDec21Key"
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "myJune2021Key" 
 }
 
-variable "instance_type" {
-  description = "instance type for ec2"
-  default     =  "t2.micro"
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
 }
 
-variable "security_group" {
-  description = "Name of security group"
-  default     = "my-jenkins-security-group-dec-2021"
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "jenkins-sg-dec-2021" 
 }
 
-variable "tag_name" {
-  description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-020db2c14939a8efb" 
 }
-
-variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "my-s3bucket-"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
 }
