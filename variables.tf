cec89160c357402a7cf6811bd9ace62d18ab7ba5
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2021-12-27 10:35:12 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sg-dec-2021" 
+    default     = "jenkins-sgroup-dec-2021" 
 }
 
 variable "tag_name" { 
