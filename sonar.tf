d900e80ea4667e2d7de4490add9db974ae778933
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2022-01-22 10:38:11 -0600

    Create sonar.tf

---
+++ sonar.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "mySonarInstance" {
+      ami           = "ami-0b9064170e32bde34"
+
+      key_name = var.key_name
+      instance_type = "t2.micro"
+      security_groups= [ "security_sonar_group_2021"]
+      tags= {
+        Name = "sonar_instance"
+      }
+    }
+
+ resource "aws_security_group" "security_sonar_group_2021" {
+      name        = "security_sonar_group_2021"
+      description = "security group for Sonar"
+
+      ingress {
+        from_port   = 9000
+        to_port     = 9000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     # outbound from Sonar server
+      egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+      tags= {
+        Name = "security_sonar"
+      }
+    }
+
+# Create Elastic IP address for Sonar instance
+resource "aws_eip" "mySonarInstance" {
+  vpc      = true
+  instance = aws_instance.mySonarInstance.id
+tags= {
+    Name = "sonar_elastic_ip"
+  }
+}
