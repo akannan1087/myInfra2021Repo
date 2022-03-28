a7b070941da8d666edeaf8fe7ac84911510c09cb
kanmonye <58056024+kanmonye@users.noreply.github.com>
2022-03-28 17:50:41 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,9 +2,9 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
-   versioning {
-    enabled = var.versioning
-  }
+   #versioning {
+    #enabled = var.versioning
+  #}
   
   tags = var.tags
 }
