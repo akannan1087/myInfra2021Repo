449ea9165a58f0984da846f62cb2456af52aadbc
kanmonye <kanmonye@yahoo.com>
2022-03-28 20:37:16 -0500

    updated entire file

--- s3.tf
+++ s3.tf
@@ -1,11 +1,15 @@
-resource "aws_s3_bucket" "my-s3-bucketkmkm" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-  versioning_configuration {
-    status = var.versioning_configuration
-  }
-  
-  tags = var.tags
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
 }
 
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
