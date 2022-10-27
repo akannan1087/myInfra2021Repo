5e4e2baa33bf25f0df35b4e2a7b2ca757156c636
jimilondon <112893676+jimilondon@users.noreply.github.com>
2022-10-27 13:10:49 -0400

    update s3 and dynamodb name

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "mydev-tf-state-bucket07"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "my-dynamodb-table07"
   }
 }
