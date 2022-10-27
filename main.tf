b9d6023686805ed0171955cc38f5079863051dcd
jimilondon <112893676+jimilondon@users.noreply.github.com>
2022-10-27 13:02:43 -0400

    update main.tf security group

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 }
 
 #Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
+resource "aws_security_group" "my-jenkins-security-group-2022" {
   name        = var.security_group
   description = "security group for Ec2 instance"
 
@@ -46,7 +46,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  vpc_security_group_ids = [aws_security_group.my-jenkins-security-group-2022.id]
   tags= {
     Name = var.tag_name
   }
