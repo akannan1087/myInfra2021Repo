5b75141050212fba925bc352a900101a4d810df9
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2022-12-18 14:48:01 -0600

    modified per my settings

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-aws-tf-state-bucket"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "my-dynamo-db-table"
   }
 }
