b5a38e2278133e1f10ce07ba1b72039da8bad9c3
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2022-12-18 14:50:12 -0600

    modified per my region settings

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-2" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "mySep22Key" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-020db2c14939a8efb" 
+    default     = "ami-0ee23bfc74a881de5" 
 }
 variable "versioning" {
     type        = bool
