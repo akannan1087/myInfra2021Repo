64264172992efe1efe8b1df90b33fd1937768442
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2023-06-19 00:17:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "shridhar1312"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "shridhar1312"
   }
 }
